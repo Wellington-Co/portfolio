298d4664bd9ff3fbc7b081d8687473355cff2e7e
Wellington-Co <omondiwellington011@gmail.com>
2025-04-13 15:38:01 +0300

    Add custom build script for Netlify

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install Flutter
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+export PATH="$PATH:`pwd`/flutter/bin"
+flutter doctor
+
+# Run flutter build web
+flutter build web
